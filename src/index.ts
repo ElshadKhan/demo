ebb454c0f8c5cb395e46c3c8faab87df341918f4
EnterTheNinja <khanakhmedov.elshad@mail.ru>
2022-09-13 13:52:54 +0300

    init commit

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import express, {Request, Response} from 'express'
+
+const app = express()
+const port = 3003
+
+app.get('/', (req: Request, res: Response) => {
+    let helloMessage = 'Hello Incubator!!!!';
+    res.send(helloMessage)
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
