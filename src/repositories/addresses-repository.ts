c38850c3ba3b134befc6088cf669657066b953e3
EnterTheNinja <khanakhmedov.elshad@mail.ru>
2022-09-14 12:16:51 +0300

    Second commit

--- src/repositories/addresses-repository.ts
+++ src/repositories/addresses-repository.ts
@@ -3,6 +3,10 @@ import {Request, Response, Router} from "express";
 const addresses = [{id: 1, value: 'Nezalejnasti 12'},{id: 2, value: 'Selickaga 11'}]
 
 export const addressesRepository = {
-
+    findAddresses() {return addresses},
+    findAddressById(id: number) {
+        let address = addresses.find(p => p.id === id);
+        return address
+    }
 }
 
--- src/routes/addresses-router.ts
+++ src/routes/addresses-router.ts
@@ -1,15 +1,16 @@
 import {Request, Response, Router} from "express";
+import {addressesRepository} from "../repositories/addresses-repository";
 
-
+const addresses = [{id: 1, value: 'Nezalejnasti 12'},{id: 2, value: 'Selickaga 11'}]
 
 export const addressesRouter = Router({})
 
 addressesRouter.get('/', (req: Request, res: Response) => {
+    const addresses = addressesRepository.findAddresses()
     res.send(addresses)
 })
 addressesRouter.get('/:id', (req: Request, res: Response) => {
-    let address = addresses.find(p => p.id === +req.params.id);
-
+    const address = addressesRepository.findAddressById(+req.params.id)
     if (address) {
         res.send(address)
     } else {
