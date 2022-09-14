1a2cfcc7ce6fbb90c6651674379482ea16b9025e
EnterTheNinja <khanakhmedov.elshad@mail.ru>
2022-09-14 15:20:01 +0300

    Fix mistakes

--- src/repositories/addresses-repository.ts
+++ src/repositories/addresses-repository.ts
@@ -1,5 +1,3 @@
-import {Request, Response, Router} from "express";
-
 const addresses = [{id: 1, value: 'Nezalejnasti 12'},{id: 2, value: 'Selickaga 11'}]
 
 export const addressesRepository = {
--- src/routes/addresses-router.ts
+++ src/routes/addresses-router.ts
@@ -1,8 +1,6 @@
 import {Request, Response, Router} from "express";
 import {addressesRepository} from "../repositories/addresses-repository";
 
-const addresses = [{id: 1, value: 'Nezalejnasti 12'},{id: 2, value: 'Selickaga 11'}]
-
 export const addressesRouter = Router({})
 
 addressesRouter.get('/', (req: Request, res: Response) => {
